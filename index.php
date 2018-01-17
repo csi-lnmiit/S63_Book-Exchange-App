a6cad30ddbf93c46317ae6f1907a092da58898d4
Aashish Raj Gupta <argupta157@gmail.com>
2018-01-17 16:04:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 	<head>
-	<link rel="shortcut icon" type="image/png" href="favicon.png">
-	<title>Libromate</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<link rel="shortcut icon" type="image/png" href="favicon.png">
+		<title>Libromate</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</head>
 
 	<body>
@@ -41,13 +41,5 @@
 				</div><!--col-md-6 end-->
 			</div><!--row end-->
 		</div><!--outer div end-->
-<<<<<<< HEAD
-
-
-		<div prateek mittal><!--change here-->
-=======
-		<div prateek mittal new new change><!--change here-->
->>>>>>> 2e1da50f9e44b0378915eb2c8c3dfad24793f3e3
-
 	</body>
 </html>
