38d07fd28e135b8a3ea465a4d72e8f9507ff31c4
Prateek Mittal <prateekmittal0097@gmail.com>
2018-02-09 22:55:45 +0530

    add books option working

--- add.php
+++ add.php
@@ -4,15 +4,29 @@
 	if(!isset($_SESSION["user"]))
 		header("Location:index.php");
 
-    $link = mysqli_connect("localhost","slp","qwerty","libromate");
-	// Check connection
-	if($link === false) {
-    	die("ERROR: Could not connect. " . mysqli_connect_error());
-	}
+require_once('db_connect.php'); //connect to database
+//    $link = mysqli_connect("localhost","slp","qwerty","libromate");
+//	// Check connection
+//	if($link === false) {
+//    	die("ERROR: Could not connect. " . mysqli_connect_error());
+//	}
     if(isset($_POST["add"])){
-        $bookname=$_POST["bookname"];
-        $authorname=$_POST["authorname"];
-        $query=insert into $sql = "insert into addbooks (id,bookname,authorname) values ('$bookname','$authorname')";
+        
+        //generate unique user id
+        $t = microtime(true);
+        $micro = sprintf("%06d",($t - floor($t)) * 1000000);
+        $d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
+        $bid="b".substr($d->format("ymdHisu"),0,14);
+        echo $bid;
+        $status=1;
+        $owner=$_SESSION["user_id"];
+        
+        
+        $bname=$_POST["bname"];
+        $bauthor=$_POST["bauthor"];
+        
+        $query="insert into books(bid,bname,author,owner,status) values ('$bid','$bname','$bauthor','$owner',$status)";
+        
 			$result = mysqli_query($link,$query);
     }
 ?>
@@ -52,7 +66,7 @@
 	            </li>
 				<br>
 	            <p>BOOKS</p>
-	            <li><a href="#">
+	            <li><a href="add.php">
 	                <span class="glyphicon glyphicon-plus"></span>&emsp;Add</a>
 	            </li>
 	            <li><a href="#">
@@ -79,44 +93,20 @@
 
 	    
 
-<!--
-	        <?php
-
-	        $t = microtime(true);
-			$micro = sprintf("%06d",($t - floor($t)) * 1000000);
-			$d = new DateTime( date('Y-m-d H:i:s.'.$micro, $t) );
-			$user_id="u".substr($d->format("ymdHisu"),0,14);
-
-			
-	         ?>
--->
+        <!--form for adding books-->
         <div style="padding-top:10px;padding-right:500px;padding-left:400px;">
             <h2 style="color:#868899;">Add Books</h2>
         </div>
         <form action="add.php" method="post" style="padding-left:400px;padding-right:300px;padding-top:30px;">
 									<div class="input-group">
   										<span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-  										<input type="text" class="form-control" name="b_name" placeholder="Book Name">
+  										<input type="text" class="form-control" name="bname" placeholder="Book Name">
 									</div>
 									<div class="input-group">
   										<span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
-  										<input type="text" class="form-control" name="author" placeholder="Author name">
+  										<input type="text" class="form-control" name="bauthor" placeholder="Author name">
 									</div>
-<!--
-									<div class="input-group">
-  										<span class="input-group-addon"><i class="glyphicon glyphicon-phone"></i></span>
-  										<input type="tel" class="form-control" name="mobile" placeholder="Mobile No">
-									</div><br>
-									<div class="input-group">
-  										<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-  										<input type="text" class="form-control" name="user" placeholder="Username">
-									</div>
-									<div class="input-group">
-								      	<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-								      	<input type="password" class="form-control" name="pass" placeholder="Password">
-								    </div>
 
--->
                                     <br>
                 					<input class="btn btn-block btn-primary" type="submit" name="add" value="Add">
                                     <input class="btn btn-block btn-primary" type="reset" name="reset" value="Reset">
