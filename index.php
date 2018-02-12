fce7736f08280003ecff2e0e4ae0dfa9188c7848
Aashish Raj Gupta <argupta157@gmail.com>
2018-02-12 19:23:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -161,6 +161,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <linl rel="stylesheet" type="text/css" href="CSS/responsive">
 <script src="reload.js"></script>
 
     <style>
